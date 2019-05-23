463ce0d8e2eaf1def96a07e2bbffa77db16aeb7e
ClaudinhoPedrotti <claudio.brito9@etec.sp.gov.br>
2019-05-22 22:14:14 -0300

    22.05.2019

---
+++ app/src/main/java/br/com/etecia/appname/MainActivity.java
@@ -0,0 +1,42 @@
+package br.com.etecia.appname;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.Toolbar);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.flecha);
+        getSupportActionBar().setTitle("App Name");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(), PersonalActivity.class));
+        super.onBackPressed();
+    }
+}
